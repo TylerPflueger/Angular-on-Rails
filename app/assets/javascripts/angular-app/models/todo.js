7ef9bd7355e859dd54ab49a097361b8b8c62d85c
Tyler Pflueger <tyler.pflueger@aviture.us.com>
2015-01-29 23:42:59 -0600

    Add API call
    Fix API call

--- app/assets/javascripts/angular-app/models/todo.js
+++ app/assets/javascripts/angular-app/models/todo.js
@@ -1,9 +1,10 @@
 (function() {
     var app = angular.module('app');
 
-    app.factory('Todo', ['$resource', function ($resource) {
-
-        var Todo = $resource('http://localhost:3000/api/v1/todos/:id.json', {id: '@id'}, {
+    app.factory('Todo', ['$location', '$resource', function ($location, $resource) {
+        var api = $location.absUrl();
+        api = api.substring(0, api.length - 2);
+        var Todo = $resource(api + 'api/v1/todos/:id.json', {id: '@id'}, {
             update: {
                 method: 'PUT'
             }
