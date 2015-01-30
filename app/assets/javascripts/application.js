42df51de59982b6c5799f5f00e05500e93ea57d0
Tyler Pflueger <tyler.pflueger@aviture.us.com>
2015-01-29 23:03:40 -0600

    More required files
    Add more required files for Heroku.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,3 +19,11 @@
 //= require angular-resource
 //= require_tree ../templates
 //= require_tree .
+//= require angular-app/app
+//= require_tree ./angular-app/templates
+//= require_tree ./angular-app/modules
+//= require_tree ./angular-app/filters
+//= require_tree ./angular-app/directives
+//= require_tree ./angular-app/models
+//= require_tree ./angular-app/services
+//= require_tree ./angular-app/controllers
